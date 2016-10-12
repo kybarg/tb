adf3a6f2fbc42874e346dbbb8cbcb6bd559b803f
Ruslan Kyba <kybargr@gmail.com>
2016-10-12 18:16:23 +0300

    Init commit.

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+
+/**
+ * Tb Class
+ */
+var tb = function () {
+
+}
+
+tb.version = require('./package.json').version;
