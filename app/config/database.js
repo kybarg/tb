cfe983dcabab71acab49c824a69dfe83ed95632b
Ruslan Kyba <kybargr@gmail.com>
2016-10-12 20:14:27 +0300

    Basic user functionality.

---
+++ app/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://tbuser:admin123@localhost:27017/tb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
--- app/index.js
+++ app/index.js
@@ -1,13 +1,66 @@
+// set up ======================================================================
+// get all the tools we need
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var logger = require('express-logger');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var exphbs = require('express-handlebars');
+
+var configDB = require('./config/database.js');
 
 
-/**
- * Tb Class
- */
-var tb = function () {
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-}
+require('./config/passport')(passport); // pass passport for configuration
 
-tb.version = require('./package.json').version;
+// set up our express application
+app.use(logger({
+    path: "./logfile.txt"
+})); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+app.use(bodyParser.urlencoded({
+        extended: false
+    })) // get information from html forms
+app.use(bodyParser.json());
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+})); // set up handlebars for templating
+app.set('view engine', 'handlebars');
+
+// required for passport
+app.use(session({
+    secret: 'Akn0gRw0395l6q4NU7rO2bbyw2xER444', // session secret
+    cookie: {
+        maxAge: 2628000000
+    },
+    store: new(require('express-sessions'))({
+        storage: 'mongodb',
+        instance: mongoose, // optional
+        host: 'localhost', // optional
+        port: 27017, // optional
+        db: 'tb', // optional
+        collection: 'sessions', // optional
+        expire: 86400 // optional
+    }),
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
