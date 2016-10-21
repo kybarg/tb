ef53d0adbbdb9e35a211db285e41f605b8579f33
Ruslan Kyba <kybargr@gmail.com>
2016-10-21 18:19:36 +0300

    Admin area (#31)
    * Fixed categories search
    
    * Foxed categories
    
    * Fixed abcestors
    
    * More fixes
    
    Added wysiwyg editor.
    Changed alias to slug.
    Removed unnecessary type conversion.
    Styled delete modal.
    
    * Fixes and vendor views
    
    * Mega mind!
    
    * Category sorting

--- app/routes/admin/category.js
+++ app/routes/admin/category.js
@@ -6,13 +6,14 @@ var path = require('path');
 var crypto = require('crypto');
 var Category = require('../../models/category');
 var pictPath = require('../../config/path.js').categoryPictPath;
+var math = require('mathjs');
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, pictPath)
     },
-    filename: function (req, file, cb) {
-        crypto.pseudoRandomBytes(16, function (err, raw) {
+    filename: function(req, file, cb) {
+        crypto.pseudoRandomBytes(16, function(err, raw) {
             if (err) return cb(err)
             cb(null, raw.toString('hex') + Date.now().toString() + path.extname(file.originalname))
         })
@@ -23,43 +24,56 @@ var upload = multer({
     storage: storage
 });
 
-module.exports = function (app, passport, exphbs) {
+module.exports = function(app, passport, exphbs) {
 
-    app.get('/admin/category/index', isLoggedIn, function (req, res) {
+    app.get('/admin/category/index', isLoggedIn, function(req, res) {
 
         req.breadcrumbs('Categories');
 
-        // Using paginate for simplier pagination
-
-        Category.paginate({}, {
-            page: req.query.page ? req.query.page : 1,
-            sort: {
-                name: 1,
+        Category.find()
+            .select({
                 ancestors: 1,
-                parent: 1,
-            },
-            limit: 50
-        }, function (err, result) {
-            if (!err) {
+                name: 1,
+                parent: 1
+            })
+            .sort('name')
+            .lean()
+            .exec(function(err, categories) {
+                if (err) throw err;
+
+                categories = treeify(categories, '_id', 'parent', 'children')
+
+                var page = parseInt(req.query.page ? req.query.page : 1);
+                var limit = parseInt(50);
+
+
+                var result = [];
+                bfs({
+                    "children": categories
+                }, 'children', result);
+
+                var pages = Math.ceil(result.length / limit);
+
+                var start = ((page - 1) * limit);
+                result = result.slice(start, (start + limit));
+
                 res.render('category/index', {
                     breadcrumbs: req.breadcrumbs(),
-                    categories: result.docs,
+                    categories: result,
                     pagination: {
-                        page: result.page,
-                        pageCount: result.pages
+                        page: page,
+                        pageCount: pages
                     },
-                    showPagination: result.pages > 1 ? true : false,
+                    showPagination: (pages > 1) ? true : false,
                     helpers: {
                         paginate: handlebarsPaginate
                     }
                 });
-            } else {
-                throw err;
-            }
-        });
+
+            });
     });
 
-    app.get('/admin/category/create', isLoggedIn, function (req, res) {
+    app.get('/admin/category/create', isLoggedIn, function(req, res) {
         req.breadcrumbs([{
             name: 'Categories',
             url: '/admin/category/index'
@@ -72,13 +86,13 @@ module.exports = function (app, passport, exphbs) {
         });
     });
 
-    app.post('/admin/category/create', isLoggedIn, upload.single('image'), function (req, res) {
+    app.post('/admin/category/create', isLoggedIn, upload.single('image'), function(req, res) {
         var category = new Category(req.body.category);
         if (req.file) {
             category.picture = req.file.filename; // Store uploaded picture filename
         }
 
-        category.save(function (err, category) {
+        category.save(function(err, category) {
             if (err) {
                 var c = new Category(req.body.category)
                 res.render('category/create', {
@@ -92,11 +106,11 @@ module.exports = function (app, passport, exphbs) {
         });
     });
 
-    app.get('/admin/category/update/:id', isLoggedIn, function (req, res) {
+    app.get('/admin/category/update/:id', isLoggedIn, function(req, res) {
         Category.findById(req.params.id)
             // .populate('ancestors')
             .populate('parent')
-            .exec(function (err, category) {
+            .exec(function(err, category) {
                 req.breadcrumbs([{
                     name: 'Categories',
                     url: '/admin/category/index'
@@ -112,11 +126,11 @@ module.exports = function (app, passport, exphbs) {
     });
 
     // Update category with ID
-    app.post('/admin/category/update/:id', isLoggedIn, upload.single('image'), function (req, res) {
+    app.post('/admin/category/update/:id', isLoggedIn, upload.single('image'), function(req, res) {
         // Check if posting new picture
         if (req.file) {
             // Check if posting new picture
-            fs.access(pictPath, fs.F_OK, function (err) {
+            fs.access(pictPath, fs.F_OK, function(err) {
                 if (!err) {
                     // Delete old picture
                     fs.unlink(pictureOldPath);
@@ -129,13 +143,13 @@ module.exports = function (app, passport, exphbs) {
             req.body.category.picture = req.file.filename;
         }
 
-        Category.findById(req.params.id, function (err, category) {
+        Category.findById(req.params.id, function(err, category) {
             if (err) throw err;
 
             // Update category object with new values
             category = Object.assign(category, req.body.category);
 
-            category.save(function (err) {
+            category.save(function(err) {
                 if (err) throw err;
                 res.redirect('/admin/category/update/' + req.params.id);
             });
@@ -143,13 +157,13 @@ module.exports = function (app, passport, exphbs) {
 
     });
 
-    app.get('/admin/category/delete/:id', isLoggedIn, function (req, res) {
+    app.get('/admin/category/delete/:id', isLoggedIn, function(req, res) {
 
         Category.findOne({
             _id: mongoose.Types.ObjectId(req.params.id)
-        }, function (err, category) {
+        }, function(err, category) {
             if (err) throw err;
-            category.remove(function (err) {
+            category.remove(function(err) {
                 if (err) throw err;
 
                 res.redirect('/admin/category/index');
@@ -157,7 +171,7 @@ module.exports = function (app, passport, exphbs) {
         });
     });
 
-    app.post('/admin/category/search', function (req, res) {
+    app.post('/admin/category/search', function(req, res) {
         Category.find({
                 name: {
                     $regex: req.body.searchString ? req.body.searchString : '',
@@ -167,7 +181,7 @@ module.exports = function (app, passport, exphbs) {
             .populate('ancestors')
             .populate('parent')
             .limit(parseInt(req.body.limit))
-            .exec(function (err, docs) {
+            .exec(function(err, docs) {
                 if (err) throw err;
                 res.send(docs);
             });
@@ -184,3 +198,37 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 };
+
+function treeify(list, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'id';
+    if (!parentAttr) parentAttr = 'parent';
+    if (!childrenAttr) childrenAttr = 'children';
+    var treeList = [];
+    var lookup = {};
+
+    var tmpList = [];
+
+    list.forEach(function(obj) {
+        lookup[obj[idAttr]] = obj;
+        obj[childrenAttr] = [];
+    });
+
+    list.forEach(function(obj) {
+        if (typeof obj[parentAttr] !== 'undefined' && obj[parentAttr] != null) {
+            lookup[obj[parentAttr]][childrenAttr].push(obj);
+        } else {
+            treeList.push(obj);
+        }
+    });
+    return treeList;
+};
+
+function bfs(tree, key, collection) {
+    if (!tree[key] || tree[key].length === 0) return;
+    for (var i = 0; i < tree[key].length; i++) {
+        var child = tree[key][i]
+        collection.push(child);
+        bfs(child, key, collection);
+    }
+    return;
+}
