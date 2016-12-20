abee6cd81c4562d4f935c09edda440ee52ae317c
Den1258 <dn.den1@gmail.com>
2016-12-20 03:12:41 +0200

    1

---
+++ app/routes/admin/import.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+var fs = require('fs');
+var async = require('async');
+var XmlStream = require('xml-stream');
+var FeedImport = require('../../import/feed_import');
+var ruleList = require('../../import/import_rules');
+var feedList = new Map();
+
+
+module.exports = function (app, passport, exphbs) {
+    app.get('/admin/import/index', isLoggedIn, function (req, res){
+
+    })
+
+    app.get('/admin/import/start', isLoggedIn, function (req, res) {
+      
+        var feed = new FeedImport('http://export.admitad.com/ru/webmaster/websites/307243/products/export_adv_products/?feed_id=4102&code=43ade2cde4&last_import=&user=jacks&format=xml&limit=100');
+  
+
+        feed.downloadFeed(null, function(){feed.startImport()});
+
+
+    })
+
+    app.get('/admin/import/rules', isLoggedIn, function (req, res) {
+        
+        
+        //res.send(feedList.product);
+    })
+    
+    app.post('/admin/import/rules/add', isLoggedIn, function (req, res) {
+        
+     
+    })
+
+    app.post('/admin/import/rules/remove', isLoggedIn, function (req, res) {
+        
+       //not yet 
+       
+    })
+}
+
+
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
